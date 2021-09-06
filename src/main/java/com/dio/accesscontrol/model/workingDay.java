b0e53541ff58625ffd8f57d3808096f3b765b963
laiscarvalho <lais.soares.carvalho@gmail.com>
2021-09-06 14:59:25 -0300

    initial commit

---
+++ src/main/java/com/dio/accesscontrol/model/workingDay.java
@@ -0,0 +1,13 @@
+package com.dio.accesscontrol.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class workingDay {
+    private long id;
+    private String description;
+}
